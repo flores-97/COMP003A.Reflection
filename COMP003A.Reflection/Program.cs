54c8bf56ab11dbae2cdb541408f29b674e904b4e
flores-97 <floresvictor15@gmail.com>
2025-02-17 16:23:31 -0800

    Add project files.

---
+++ COMP003A.Reflection/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace COMP003A.Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. What worked well for you over the last 8 weeks?");
+
+            Console.WriteLine("2. What external resources did you use to aid your learning?");
+
+            Console.WriteLine("3. What would you change about the course? ");
+
+            Console.WriteLine("4. Did you learn what you expected to learn in this course?");
+
+            Console.WriteLine("5. What concepts were new to you?");
+
+            Console.WriteLine("6. What concepts have you encountered already in the past?");
+
+            Console.WriteLine("7. Do you have any tips/comments for the instructor?");
+        }
+    }
+}
